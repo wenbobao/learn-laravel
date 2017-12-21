9f7452e07d5d3c27e1b8f3d04d56d9ae0f8d9dd4
wenbobao <744865006@qq.com>
2017-12-21 15:52:07 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,10 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <title>@yield('title', 'Sample')</title>
+	<meta charset="UTF-8">
+	<title>@yield('title', 'Sample')</title>
+	<link rel="stylesheet" href="/css/app.css">
 </head>
+
 <body>
-@yield('content')
+	@include('layouts._header')
+	<div class="container">
+		<div class="col-md-offset-1 col-md-10">
+			@yield('content')
+            @include('layouts._footer')
+		</div>
+	</div>
 </body>
+
 </html>
