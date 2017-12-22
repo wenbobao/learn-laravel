f2aa7590e6d84455d9d4bfcc84bc1e3e8608d0c6
wenbobao <744865006@qq.com>
2017-12-22 15:19:24 +0000

    Finish login and logout

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Auth;
 
 class UsersController extends Controller
 {
@@ -29,8 +30,9 @@ class UsersController extends Controller
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' => $request->password,
+            'password' => bcrypt($request->password),
         ]);
+        Auth::login($user);
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
     }
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -15,7 +15,8 @@
             @yield('content')
             @include('layouts._footer')
 		</div>
-	</div>
+    </div>
+    <script src="/js/app.js"></script>
 </body>
 
 </html>
